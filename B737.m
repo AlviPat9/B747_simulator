afd24e3fb65b8a7436303562fa4d82e648c4c4f7
AlviPat9 <79159554+AlviPat9@users.noreply.github.com>
2023-01-19 10:22:08 +0100

    Update B737.m

--- B737.m
+++ B737.m
@@ -14,7 +14,7 @@ I = [18200000, 0, -970000; 0, 33100000, 0; -970000, 0, 49700000] * Units.slugft2
 M = 0.65;
 x0 = [0; 0; 20000*0.3048];
 atm = atmospheric_model(x0(3));
-v = [atm(4)*0.65, 0, atm(4)*0.65*sind(2.5)];
+
 % Steady state coeff
 CLs = 0.4; CDs = 0.025; CTs = 0.025;
 fun = @(x)StationaryState(x,x0(3),M);
@@ -36,11 +36,11 @@ de=x(3);
 atm = atmospheric_model(h);
 %% Longitudinal coefficients
 % Drag Coefficients
-CD0 = 0.0164;CD_a = -0.0038;
+CD0 = 0.0164;CD_a = -0.2;
 % Lift Coefficients
-CL0 = 0.21;CL_a = 5.9111;CL_de = 0.32;
+CL0 = 0.21;CL_a = 4.4;CL_de = 0.32;
 % Pitch Coefficients
-Cm_0 = 0;Cm_a = -2.8536;Cm_de = -1.3;
+Cm_0 = 0;Cm_a = -1.0;Cm_de = -1.3;
 %% geometry
 c = 27.3 * 0.3048;
 b = 196 * 0.3048;
@@ -52,7 +52,7 @@ qdyn = 0.5*atm(3)*us^2;
 
 T_max=275.8*1000;
 
-T =4*T_max*M*cos(alpha)*T_lever^4;
+T =4*T_max*M*cos(alpha)*T_lever;
 D = qdyn * S * (CD0+CD_a*alpha);
 L = qdyn * S * (CL0+CL_a*alpha+CL_de*de);
 out(1) = T+L*sin(alpha)-D*cos(alpha)-W*9.81*sin(alpha);
