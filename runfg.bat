7368c322d5f5e9e8628e4b297acd98e5b51b0d22
AlviPat9 <79159554+AlviPat9@users.noreply.github.com>
2023-04-10 23:51:16 +0200

    Updated model

---
+++ runfg.bat
@@ -0,0 +1,7 @@
+D:
+
+cd D:\FlightGear 2020.3
+
+SET FG_ROOT=D:\FlightGear 2020.3\data
+
+START .\\bin\fgfs.exe --fdm=null --native-fdm=socket,in,30,localhost,5502,udp    --aircraft=747-400 --fog-fastest --disable-clouds --start-date-lat=2004:06:01:09:00:00 --disable-sound --in-air --airport=KSFO --runway=10L --altitude=10668 --heading=0 --offset-distance=4.72 --offset-azimuth=0  
